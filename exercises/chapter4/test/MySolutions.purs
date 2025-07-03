68e12da70d9aebb5200dbb1c462677e9d0e9294e
NEKN3X <43030839+NEKN3X@users.noreply.github.com>
2025-07-03 17:19:20 +0900

    add purs-tidy

--- exercises/chapter4/test/MySolutions.purs
+++ exercises/chapter4/test/MySolutions.purs
@@ -1,1 +1,14 @@
 module Test.MySolutions where
+
+import Prelude
+
+factorial :: Int -> Int
+factorial 0 = 1
+factorial n = n * factorial (n - 1)
+
+binomial :: Int -> Int -> Int
+binomial n k | k > n = 0
+binomial n k = factorial n `div` (factorial k * factorial (n - k))
+
+pascal :: Int -> Int -> Int
+pascal n k = binomial (n - 1) k + binomial (n - 1) (k - 1)
