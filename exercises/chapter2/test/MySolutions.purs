13248ddfd62f45a02736c650a83697cb9db85ebb
NEKN3X <43030839+NEKN3X@users.noreply.github.com>
2025-07-03 13:58:34 +0900

    [Chapter 2] Warningを解消

--- exercises/chapter2/test/MySolutions.purs
+++ exercises/chapter2/test/MySolutions.purs
@@ -5,8 +5,11 @@ import Prelude
 import Data.Int (rem)
 import Data.Number (pi, sqrt)
 
+diagonal ∷ Number → Number → Number
 diagonal w h = sqrt (w * w + h * h)
 
+circleArea ∷ Number → Number
 circleArea r = pi * r * r
+leftoverCents ∷ Int → Int
 
 leftoverCents n = rem n 100
